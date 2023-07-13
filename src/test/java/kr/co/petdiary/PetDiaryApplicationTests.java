90e187cdb44e989a371a3df7e9615f6ab01176cf
HyuckJuneHong <jjj8337721@naver.com>
2023-07-13 14:30:22 +0900

    init : 프로젝트 초기화 (java 17, spring boot 3.1.1, gradle(build tool))

---
+++ src/test/java/kr/co/petdiary/PetDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.petdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
