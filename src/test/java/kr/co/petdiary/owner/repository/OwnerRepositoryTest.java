fe324130c6f88251ce107446b5cd8397c102c109
HyuckJuneHong <jjj8337721@naver.com>
2023-07-14 17:45:12 +0900

    refactor : TestEntityManager 테스트 위치 이동

--- src/test/java/kr/co/petdiary/owner/repository/OwnerRepositoryTest.java
+++ src/test/java/kr/co/petdiary/owner/repository/OwnerRepositoryTest.java
@@ -1,20 +1,15 @@
 package kr.co.petdiary.owner.repository;
 
-import jakarta.validation.ConstraintViolationException;
 import kr.co.petdiary.global.config.JpaConfig;
 import kr.co.petdiary.owner.entity.Owner;
 import kr.co.petdiary.owner.model.OwnerCreators;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -22,9 +17,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class OwnerRepositoryTest {
     @Autowired
     private OwnerRepository ownerRepository;
-
-    @Autowired
-    private TestEntityManager testEntityManager;
 
     @Test
     void OwnerRepository가_NULL이_아님() {
@@ -42,27 +34,5 @@ class OwnerRepositoryTest {
         //then
         assertThat(actual.getId()).isNotNull();
         assertThat(actual.getEmail()).isEqualTo(saveOwner.getEmail());
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"010-12-5678", "01012345678", "0"})
-    void 전화번호_형식이_맞지_않음(String cellPhone) {
-        //given
-        final Owner actual = OwnerCreators.createOwnerByCellPhone(cellPhone);
-
-        //when, then
-        assertThatThrownBy(() -> testEntityManager.persistAndFlush(actual))
-                .isInstanceOf(ConstraintViolationException.class);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"exampenaver.com", "@naver.com", "example@"})
-    void 이메일_형식이_맞지_않음(String email) {
-        //given
-        final Owner actual = OwnerCreators.createOwnerByEmail(email);
-
-        //when, then
-        assertThatThrownBy(() -> testEntityManager.persistAndFlush(actual))
-                .isInstanceOf(ConstraintViolationException.class);
     }
 }
